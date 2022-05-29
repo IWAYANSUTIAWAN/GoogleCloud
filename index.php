9461460c02f3aabf8ab1ea3fa020ca99ed82a3d0
I Wayan Sutiawan <38216200+IWAYANSUTIAWAN@users.noreply.github.com>
2022-05-29 08:09:19 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,157 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Submision 1 Dicoding</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://storage.googleapis.com/storage-submision-1/Style.css">
+<link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Roboto'>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<style>
+html,body,h1,h2,h3,h4,h5,h6 {font-family: "Roboto", sans-serif}
+</style>
+</head>
+<body class="w3-light-grey">
+
+<!-- Page Container -->
+<div class="w3-content w3-margin-top" style="max-width:1400px;">
+
+  <!-- The Grid -->
+  <div class="w3-row-padding">
+  
+    <!-- Left Column -->
+    <div class="w3-third">
+    
+      <div class="w3-white w3-text-grey w3-card-4">
+        <div class="w3-display-container">
+         <a href="https://storage.googleapis.com/storage-submision-1/ID%20Card.jpg" target="_blank"> <img  src="https://storage.googleapis.com/storage-submision-1/ID%20Card.jpg" style="width:100%" alt="Avatar"> </a>
+          <div class="w3-display-bottomleft w3-container w3-text-black">
+            
+          </div>
+        </div>
+        <div class="w3-container">
+  
+          <p><i class="fa  fa-fw w3-margin-right w3-large w3-text-teal"></i>I Wayan Sutiawan</p>
+          <p><i class="fa  fa-fw w3-margin-right w3-large w3-text-teal"></i>wayan.sutiawan89@gmail.com</p>
+          <p><i class="fa  fa-fw w3-margin-right w3-large w3-text-teal"></i>085738779154</p>
+          <p><i class="fa  fa-fw w3-margin-right w3-large w3-text-teal"></i>Denpasar-Bali</p>
+          <hr>
+
+          <p class="w3-large"><b><i class="fa fa-asterisk fa-fw w3-margin-right w3-text-teal"></i>Skills</b></p>
+          <p>Mikrotik</p>
+          <div class="w3-light-grey w3-round-xlarge w3-small">
+            <div class="w3-container w3-center w3-round-xlarge w3-teal" style="width:90%">90%</div>
+          </div>
+          <p>Google Cloud</p>
+          <div class="w3-light-grey w3-round-xlarge w3-small">
+            <div class="w3-container w3-center w3-round-xlarge w3-teal" style="width:80%">
+              <div class="w3-center w3-text-white">80%</div>
+            </div>
+          </div>
+          <p>Illustrator</p>
+          <div class="w3-light-grey w3-round-xlarge w3-small">
+            <div class="w3-container w3-center w3-round-xlarge w3-teal" style="width:75%">75%</div>
+          </div>
+          <p>Excel</p>
+          <div class="w3-light-grey w3-round-xlarge w3-small">
+            <div class="w3-container w3-center w3-round-xlarge w3-teal" style="width:50%">50%</div>
+          </div>
+          <br>
+
+          <p class="w3-large w3-text-theme"><b><i class="fa fa-globe fa-fw w3-margin-right w3-text-teal"></i>Languages</b></p>
+          <p>English</p>
+          <div class="w3-light-grey w3-round-xlarge">
+            <div class="w3-round-xlarge w3-teal" style="height:24px;width:100%"></div>
+          </div>
+          <p>Spanish</p>
+          <div class="w3-light-grey w3-round-xlarge">
+            <div class="w3-round-xlarge w3-teal" style="height:24px;width:55%"></div>
+          </div>
+         
+          <br>
+        </div>
+      </div><br>
+
+    <!-- End Left Column -->
+    </div>
+
+    <!-- Right Column -->
+    <div class="w3-twothird">
+    
+      <div class="w3-container w3-card w3-white w3-margin-bottom">
+        <h2 class="w3-text-grey w3-padding-16"><i class="fa fa-envelope fa-fw w3-margin-right w3-xxlarge w3-text-teal"></i>About Me</h2>
+        <div class="w3-container">
+          
+	  
+          <p>Saya ada seorang IT Hardware yang saat ini bekerja sebagai salah satu tim IT di perusahaan farmasi indonesia, disela sela kesibukan saya selalu 
+		mengkuti  perkembangan teknologi salah satunya adalah dibidang cloud</p>
+          <hr>
+        </div>
+       
+      </div>
+	<div class="w3-container w3-card w3-white w3-margin-bottom">
+
+        <h2 class="w3-text-grey w3-padding-16"><i class="fa fa-suitcase fa-fw w3-margin-right w3-xxlarge w3-text-teal"></i>Work Experience</h2>
+        <div class="w3-container">
+          <h5 class="w3-opacity"><b>IT Hardware Supervisor  | PT Sumber Hidup Sehat</b></h5>
+	  <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i>Jan 2017 - <span class="w3-tag w3-teal w3-round">Current</span></h6>
+          <p>Setup Hardware, internet, dan jaringan dan memberikan support kepada tim divisi lain terkait permasalahan IT</p>
+          <hr>
+        </div>
+        <div class="w3-container">
+          <h5 class="w3-opacity"><b>IT Staff | PT Gagan Indonesia</b></h5>
+	 <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i>Mar 2012 - Dec 2014</h6>
+          <p>Setup Hardware dan instal aplikasi baru serta monitoring sinkronisasi data outlet dengan Kantor Pusat</p>
+          <hr>
+        </div>
+        <div class="w3-container">
+          <h5 class="w3-opacity"><b>IT Support | CV Hartono Indoperkasa</b></h5>
+	  <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i>Jun 2010 - Mar 2012</h6>
+          <p>Membangun aplikasi website menggunakan Laravel </p><br>
+        </div>
+      </div>
+
+	<div class="w3-container w3-card w3-white w3-margin-bottom">
+        
+       
+       
+      </div>
+
+
+      <div class="w3-container w3-card w3-white">
+        <h2 class="w3-text-grey w3-padding-16"><i class="fa fa-certificate fa-fw w3-margin-right w3-xxlarge w3-text-teal"></i>Education</h2>
+        <div class="w3-container">
+          <h5 class="w3-opacity"><b>STIKOM BALI</b></h5>
+          <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i>2008-2013</h6>
+         
+        </div>
+        <div class="w3-container">
+          <h5 class="w3-opacity"><b>WEARNES EDUCATION CENTER</b></h5>
+          <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i>2007-2008</h6>
+        
+        </div>
+       
+      </div>
+
+    <!-- End Right Column -->
+    </div>
+    
+  <!-- End Grid -->
+  </div>
+  
+  <!-- End Page Container -->
+</div>
+
+<footer class="w3-container w3-teal w3-center w3-margin-top">
+  <p>Find me on social media.</p>
+  <i class="fa fa-facebook-official w3-hover-opacity"></i>
+  <i class="fa fa-instagram w3-hover-opacity"></i>
+  <i class="fa fa-snapchat w3-hover-opacity"></i>
+  <i class="fa fa-pinterest-p w3-hover-opacity"></i>
+  <i class="fa fa-twitter w3-hover-opacity"></i>
+  <i class="fa fa-linkedin w3-hover-opacity"></i>
+  <p>Powered by <a href="" target="_blank">Wayan Sutiawan</a></p>
+</footer>
+
+</body>
+</html>
